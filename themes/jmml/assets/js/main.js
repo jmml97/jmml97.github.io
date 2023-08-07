2d58732ac9312f4a2dd45b1a584c404273cdd0a4
José María Martín <jmmartinluque@gmail.com>
2023-08-07 22:39:03 +0200

    feat: Enable multiple galleries per page

--- themes/jmml/assets/js/main.js
+++ themes/jmml/assets/js/main.js
@@ -9,18 +9,19 @@ import lazysizes from "jslibs/lazysizes/lazysizes.js";
 Alpine.plugin(collapse);
 Alpine.start();
 
-const gallery = document.querySelector(".fj-gallery");
+const galleries = document.querySelectorAll(".fj-gallery");
 
-if (gallery) {
-  const lightbox = new PhotoSwipeLightbox({
-    gallery,
-    children: ".fj-gallery-item",
-    showHideAnimationType: "zoom",
-    bgOpacity: 1,
-    pswpModule: PhotoSwipe,
-    imageClickAction: "close",
-    paddingFn: (viewportSize) => {
-      return viewportSize.x < 700
+galleries.forEach((gallery) => {
+  if (gallery) {
+    const lightbox = new PhotoSwipeLightbox({
+      gallery,
+      children: ".fj-gallery-item",
+      showHideAnimationType: "zoom",
+      bgOpacity: 1,
+      pswpModule: PhotoSwipe,
+      imageClickAction: "close",
+      paddingFn: (viewportSize) => {
+        return viewportSize.x < 700
         ? {
             top: 0,
             bottom: 0,
@@ -33,60 +34,61 @@ if (gallery) {
             left: 0,
             right: 0,
           };
-    },
-  });
-
-  lightbox.on("uiRegister", () => {
-    lightbox.pswp.ui.registerElement({
-      name: "download-button",
-      order: 8,
-      isButton: true,
-      tagName: "a",
-      html: {
-        isCustomSVG: true,
-        inner: '<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
-        outlineID: "pswp__icn-download",
-      },
-      onInit: (el, pswp) => {
-        el.setAttribute("download", "");
-        el.setAttribute("target", "_blank");
-        el.setAttribute("rel", "noopener");
-        el.setAttribute("title", "Download");
-        pswp.on("change", () => {
-          el.href = pswp.currSlide.data.element.href;
-        });
       },
     });
-  });
 
-  lightbox.on("change", () => {
-    history.replaceState("", document.title, "#" + lightbox.pswp.currSlide.index);
-  });
+    lightbox.on("uiRegister", () => {
+      lightbox.pswp.ui.registerElement({
+        name: "download-button",
+        order: 8,
+        isButton: true,
+        tagName: "a",
+        html: {
+          isCustomSVG: true,
+          inner: '<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
+          outlineID: "pswp__icn-download",
+        },
+        onInit: (el, pswp) => {
+          el.setAttribute("download", "");
+          el.setAttribute("target", "_blank");
+          el.setAttribute("rel", "noopener");
+          el.setAttribute("title", "Download");
+          pswp.on("change", () => {
+            el.href = pswp.currSlide.data.element.href;
+          });
+        },
+      });
+    });
 
-  lightbox.on("close", () => {
-    history.replaceState("", document.title, window.location.pathname);
-  });
+    lightbox.on("change", () => {
+      history.replaceState("", document.title, "#" + lightbox.pswp.currSlide.index);
+    });
 
-  new PhotoSwipeDynamicCaption(lightbox, {
-    mobileLayoutBreakpoint: 700,
-    type: "auto",
-    mobileCaptionOverlapRatio: 1,
-  });
+    lightbox.on("close", () => {
+      history.replaceState("", document.title, window.location.pathname);
+    });
 
-  fjGallery(gallery, {
-    itemSelector: ".fj-gallery-item",
-    onInit: () => {
-      gallery.style.visibility = "";
-      lightbox.init();
-    },
-    onJustify: () => {
-      if (window.location.hash.substring(1).length > 0) {
-        const index = parseInt(window.location.hash.substring(1), 10);
-        if (!Number.isNaN(index) && index >= 0 && index < gallery.querySelectorAll("a").length) {
-          lightbox.loadAndOpen(index, { gallery });
+    new PhotoSwipeDynamicCaption(lightbox, {
+      mobileLayoutBreakpoint: 700,
+      type: "auto",
+      mobileCaptionOverlapRatio: 1,
+    });
+
+    fjGallery(gallery, {
+      itemSelector: ".fj-gallery-item",
+      onInit: () => {
+        gallery.style.visibility = "";
+        lightbox.init();
+      },
+      onJustify: () => {
+        if (window.location.hash.substring(1).length > 0) {
+          const index = parseInt(window.location.hash.substring(1), 10);
+          if (!Number.isNaN(index) && index >= 0 && index < gallery.querySelectorAll("a").length) {
+            lightbox.loadAndOpen(index, { gallery });
+          }
         }
-      }
-    },
-    transitionDuration: false,
-  });
-}
+      },
+      transitionDuration: false,
+    });
+  }
+});
